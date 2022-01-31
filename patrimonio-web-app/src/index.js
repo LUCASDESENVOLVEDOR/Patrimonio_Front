d88e048cdb34be0b3e081df9fe533e0fc741574d
lucasdesenvolvedor <lucas_aragao_fel@hotmail.com>
2022-01-31 08:47:30 -0300

    base gufi

--- patrimonio-web-app/src/index.js
+++ patrimonio-web-app/src/index.js
@@ -1,15 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import { parseJwt, usuarioAutenticado } from './services/auth';
+
 import './index.css';
-import App from './App';
+
+import Home from './pages/home/App';
+import Login from './pages/login/login';
+import TiposEventos from './pages/tiposEventos/TiposEventos.jsx';
+import EventosAdm from './pages/eventosAdm/eventosAdm';
+import MeusEventos from './pages/meusEventos/meusEventos';
+import TiposUsuarios from './pages/tiposUsuarios/tiposUsuarios';
+import Perfil from './pages/perfil/perfil';
+import NotFound from './pages/notFound/NotFound';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const PermissaoAdm = ({ component: Component }) => (
+  <Route
+    render={(props) =>
+      usuarioAutenticado() && parseJwt().role === '1' ? (
+        // operador spread
+        <Component {...props} />
+      ) : (
+        <Redirect to="login" />
+      )
+    }
+  />
 );
+
+const PermissaoComum = ({ component: Component }) => (
+  <Route
+    render={(props) =>
+      usuarioAutenticado() && parseJwt().role === '2' ? (
+        // operador spread
+        <Component {...props} />
+      ) : (
+        <Redirect to="login" />
+      )
+    }
+  />
+);
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} /> {/* Home */}
+        <Route path="/login" component={Login} /> {/* Login */}
+        <PermissaoAdm path="/tiposEventos" component={TiposEventos} /> {/* Tipos Eventos */}
+        <PermissaoAdm path="/tiposUsuarios" component={TiposUsuarios} /> {/* Tipos Usuários */}
+        <PermissaoAdm path="/eventosAdm" component={EventosAdm} /> {/* Eventos Adm */}
+        <PermissaoComum path="/meusEventos" component={MeusEventos} /> {/* Meus Eventos */}
+        <Route path="/perfil" component={Perfil} /> {/* Perfil */}
+
+        <Route path="/notFound" component={NotFound} /> {/* Not Found */}
+        <Redirect to="/notFound" /> {/* Redireciona para Not Found caso não encontre nenhuma rota */}
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
